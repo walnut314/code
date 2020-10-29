40e7d94ae7234ec9976347a1540621fa9adc4fc2
Bart Bartel <v-babart@microsoft.com>
2020-10-29 09:20:45 -0700

    kd automation stuff

---
+++ work/dbg/cmds/anal.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+#
+{
+    local @ARGV = qw<param1 param2 param3>;
+    system ('analdump.cmd &');
+}
---
+++ work/dbg/cmds/analdump.cmd
@@ -0,0 +1,4 @@
+rem "\Program Files (x86)\Windows Kits\10\Debuggers\x64\kd.exe" -z "LiveDump3.dmp" -c "$$< .\power.kd"
+rem 
+"C:\Debuggers\uncompressed\amd64\kd.exe" -z "LiveDump3.dmp" -c "$$< .\power.kd"
+
