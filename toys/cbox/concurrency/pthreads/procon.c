9c1541867fd79d7a5ca7d2849f84e62b345cb73f
Bart Bartel <v-babart@microsoft.com>
2020-11-02 11:00:21 -0800

    conc

--- toys/cbox/concurrency/pthreads/procon.c
+++ toys/cbox/concurrency/pthreads/procon.c
@@ -2,75 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #define BUF_SIZE 5
 
-// the buffer works like a stack for
-// the sake of simplicity, if needed
-// we may implement a queue
 typedef struct {
-    int buf[BUF_SIZE]; // the buffer
+    int buf[BUF_SIZE]; // the buffer acts like a stack, if needed implement a queue
     size_t len; // number of items in the buffer
     pthread_mutex_t mutex; // needed to add/remove data from the buffer
     pthread_cond_t can_produce; // signaled when items are removed
     pthread_cond_t can_consume; // signaled when items are added
 } buffer_t;
 
-// produce random numbers
-void* producer(void *arg) {
+void* producer(void *arg) { // produce random numbers
     buffer_t *buffer = (buffer_t*)arg;
-
     while(1) {
         pthread_mutex_lock(&buffer->mutex);
         if(buffer->len == BUF_SIZE) { // full
             // wait until some elements are consumed
             pthread_cond_wait(&buffer->can_produce, &buffer->mutex);
         }
-
-        // in real life it may be some data fetched from
-        // sensors, the web, or just some I/O
         int t = rand();
         printf("%p Produced: %d\n", pthread_self(), t);
-
         // append data to the buffer
         buffer->buf[buffer->len] = t;
         ++buffer->len;
-
         // signal the fact that new items may be consumed
         pthread_cond_signal(&buffer->can_consume);
         pthread_mutex_unlock(&buffer->mutex);
     }
-
-    // never reached
-    return NULL;
+    return NULL; // never reached
 }
 
-// consume random numbers
-void* consumer(void *arg) {
+void* consumer(void *arg) { // consume random numbers
     buffer_t *buffer = (buffer_t*)arg;
-
     while(1) {
         pthread_mutex_lock(&buffer->mutex);
-
         // single consume -> use if
         //if(buffer->len == 0) { // empty
         // multiple consumers -> use while
-        while(buffer->len == 0) { // empty
-            // wait for new items to be appended to the buffer
+        while(buffer->len == 0) { // while empty, wait for new items to be appended to the buffer
             pthread_cond_wait(&buffer->can_consume, &buffer->mutex);
         }
-
-        // grab data
-        --buffer->len;
+        --buffer->len; // grab data
         printf("%p Consumed: %d\n", pthread_self(), buffer->buf[buffer->len]);
-
         // signal the fact that new items may be produced
         pthread_cond_signal(&buffer->can_produce);
         pthread_mutex_unlock(&buffer->mutex);
     }
-
-    // never reached
-    return NULL;
+    return NULL; // never reached
 }
 
 int main(int argc, char *argv[]) {
