af5e5d41af113821debfd00d76511607d159f726
Bart <Bart@Bartels-iMac.hsd1.wa.comcast.net>
2020-11-27 19:49:16 -0800

    update

--- toys/grokkage2/uva100911.cpp
+++ toys/grokkage2/uva100911.cpp
@@ -15,13 +15,28 @@ double matching(int bit_mask)
     if (bit_mask == (1 << 2 * N) - 1)
         return memo[bit_mask] = 0;
 
-    // start here
-    return 0.0;
+    double matching_value = 32767 * 32767;
+    for (int p1 = 0; p1 < 2 * N; p1++) {
+        if (!(bit_mask & (1 << p1))) { 
+            for (int p2 = p1 + 1; p2 < 2 * N; p2++)
+                if (!(bit_mask & (1 << p2)))
+                    matching_value = min(matching_value, 
+                        dist[p1][p2] + matching(bit_mask | (1 << p1) | (1 << p2)));
+            break;
+        }
+    }
+
+    return memo[bit_mask] = matching_value;
 }
 
 
 int main()
 {
+    char line[1000], name[1000];
+    int i, j, caseNo = 1, x[20], y[20];
+    while (sscanf(gets(line), "%d", &N), N) {
+    }
+
     return 0;
 }
 
