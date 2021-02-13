f412623e19183de26392cb17c7099df9e007ad72
Bart Bartel <v-babart@microsoft.com>
2021-02-13 13:43:29 -0800

    finance updates

--- toys/misc/py/finance/pdr_scan.py
+++ toys/misc/py/finance/pdr_scan.py
@@ -3,36 +3,51 @@
 from pandas_datareader import data as pdr
 import pandas as pd
 import yfinance as yf
-from datetime import datetime
+import datetime
+
+# calculate all dates
+today = datetime.date.today()
+weekday = today.weekday()
+yesterday = today - datetime.timedelta(days=1)
+start_delta = datetime.timedelta(days=weekday, weeks=1)
+lastMonday = today - start_delta
+week_delta = datetime.timedelta(days=4)
+lastFriday = lastMonday+week_delta
 
 # Tickers list
-ticker_list = ['XLK', 'SPY', 'AAPL', 'OXY', 'IVV']
+ticker_list = ['SPY', 'XLK', 'AAPL']
 
-today = datetime.today()
-# We can get data by our choice by giving days bracket
-start_date = datetime(2021, 2, 12) #'2017–01–01'
-end_date   = datetime(2021, 2, 12) #'2019–11–30'
-year = '2021'
-month = '2'
-day = '12'
-start_datum = year + '-' + month + '-' + day
-end_datum   = year + '-' + month + '-' + day
+#weekly
+start_week = lastMonday
+end_week = lastFriday
 
-def getData(ticker, start_datum, end_datum):
+#daily
+start_day = yesterday
+end_day = yesterday
+
+def getData(ticker, start_date, end_date):
     data = None
     try:
-        data = pdr.get_data_yahoo(ticker, start_datum, end_datum)
+        data = pdr.get_data_yahoo(ticker, start_date, end_date)
     finally:
         return data
 
 def main():
-    print(start_datum, ' to ', end_datum)
-    for tik in ticker_list:
-        print (tik)
-        data = getData(tik, start_datum, end_datum)
-        if data is None:
-            return
-        print(data)
+    print('week: ', start_week, ' to ', end_week)
+    print('day:  ', start_day, ' to ', end_day)
+    for ticker in ticker_list:
+        print()
+        print(ticker)
+        try:
+            week = pdr.get_data_yahoo(ticker, start_week, end_week)
+            day  = pdr.get_data_yahoo(ticker, start_day, end_day)
+        finally:
+            print()
+            if week is not None:
+                print(week)
+            print()
+            if day is not None:
+                print(day)
 
 if __name__ == "__main__":
     main()    
