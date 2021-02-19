8d1e2c147c92fdeee9988f06637fc83f275bf353
Bart Bartel <v-babart@microsoft.com>
2021-02-19 13:24:29 -0800

    updates

--- work/dbg/cmds/anal.py
+++ work/dbg/cmds/anal.py
@@ -2,10 +2,11 @@
 
 import subprocess
 
-def main():
-    print("hello, world")
+cmd_list = ['anal1.cmd','anal2.cmd','anal3.cmd','anal4.cmd','anal5.cmd','anal6.cmd','anal7.cmd','anal8.cmd']
 
-    with subprocess.Popen(["anal.cmd"], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) as p:
+def proc_dump(cmd):
+    print(cmd)
+    with subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) as p:
         std_out, errors = p.communicate() #(str.encode("utf-8"))
         if std_out:
             out = std_out.splitlines()
@@ -14,6 +15,10 @@ def main():
                     print(ln)
         if errors:
             print("Error message: {}".format(error))
+    
+def main():
+    for cmd in cmd_list:
+        proc_dump(cmd)
 
 main()
 
