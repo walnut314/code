1a04691ed70710412b9a8e7431badfa74ff92222
Bart Bartel <v-babart@microsoft.com>
2021-03-30 16:12:06 -0700

    update test app

--- work/uefi/uefi.c
+++ work/uefi/uefi.c
@@ -5,15 +5,63 @@
 // https://docs.microsoft.com/en-us/windows/win32/sysinfo/access-uefi-firmware-variables-from-a-universal-windows-app
 // https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-enumsystemfirmwaretables
 // https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemfirmwaretable
+// https://www.codeguru.com/cpp/misc/misc/system/article.php/c12347/SMBIOS-Demystified.htm
+// https://gist.github.com/Meitinger/65d321765590f85c70bccdf12bccab16
 
 #include <stdio.h>
 #include <windows.h>
-#include <tchar.h>
+//#include <tchar.h>
 #include <stdio.h>
-#include <strsafe.h>
+//#include <strsafe.h>
+#include <string.h>
 
 #define UEFI_VARIABLE_RUNTIME_MODE      (L"CurrentMsSurfaceUefiRuntimeModeVariableName")
 #define UEFI_MFG_GUID                   (L"{3811BE0C-6AEF-44DD-9382-CD99A24C6619}")
+
+void hexdump(const unsigned char *buffer, unsigned long long length)
+{
+    char OffsetBuf[10];
+    unsigned long long LastStart = 0, i, k, j;
+    unsigned long long elf_bias = -62;
+    for (i = 0 ; i < length ; i++) {
+        if (i % 16 == 0) {
+            sprintf(OffsetBuf, "%08llx", i);
+            printf("%4s:", OffsetBuf);
+        }
+        if (i % 4 == 0) {
+            printf(" ");
+        }
+        printf("%02x ", buffer[i]);
+        if (i % 16 == 15 || i == length-1) {
+            if (i == length-1) {
+                for (k = i % 16 ; k < 15 ; k++) {
+                     printf( "   " );
+                     if (k % 4 == 3 && k % 16 != 0) {
+                         printf(" ");
+                     }
+                }
+            }
+            printf("|");
+            for (j = LastStart ; j <= i ; j++) {
+                if (j % 4 == 0) {
+                    printf(" ");
+                }
+                if (buffer[j] > 31 && buffer[j] < 127) {
+                    printf("%c", buffer[j]);
+                } else {
+                    printf(".");
+                }
+            }
+            if (i == length-1) {
+                printf("\n");
+                break;
+            }
+            printf("\n");
+            LastStart += 16;
+        }        
+    }
+}
+
 
 void do_uefi()
 {
@@ -29,9 +77,11 @@ void do_uefi()
 }
 
 #define BUFSIZE 256
-#define FAC_TABLE_TAG (DWORD) 'PCAF'
-#define ACPI_SIG (DWORD) 'ACPI'
-
+#define FAC_TABLE_TAG   ((DWORD) 'PCAF')
+#define ACPI_SIG        ((DWORD) 'ACPI')
+#define DSDT_SIG        ((DWORD) 'DSDT')
+#define ACPI 'ACPI'
+ 
 int main(int argc, char *argv[])
 {
     const DWORD Signature = ACPI_SIG;
@@ -42,13 +92,15 @@ int main(int argc, char *argv[])
     printf("%s\n", pBuffer);
 
     printf("sig %x\n", Signature);
-    ret = EnumSystemFirmwareTables(Signature, (PVOID) pBuffer, BUFSIZE);
+    ret = EnumSystemFirmwareTables('ACPI', (PVOID) pBuffer, BUFSIZE);
+    //ret = EnumSystemFirmwareTables('RSMB', (PVOID) pBuffer, BUFSIZE);
     printf("enum %s ret %d\n", pBuffer, ret);
-    memset(pBuffer, 0, BUFSIZE);
-    ret = GetSystemFirmwareTable(Signature, 'DSDT', (PVOID) pBuffer, BUFSIZE);
-    printf("tbl %s ret %d\n", pBuffer, ret);
+    hexdump(pBuffer, ret);
+    //memset(pBuffer, 0, BUFSIZE);
+    //ret = GetSystemFirmwareTable(Signature, DSDT_SIG, (PVOID) pBuffer, BUFSIZE);
+    //printf("tbl %s ret %d\n", pBuffer, ret);
 
-    do_uefi();
+    //do_uefi();
     return 0;
 }
 
