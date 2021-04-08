0f35add3cf63d420ba5fb6552512eed484f50483
Bart Bartel <v-babart@microsoft.com>
2021-04-08 15:01:38 -0700

    the guide

---
+++ toys/grokkage/the_guide/tut.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int id;
+    cin >> id;
+    cout << "dude, sup: " << id << endl;
+    printf("yo homi\n");
+    return 0;
+}
+
