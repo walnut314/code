7d47dad523e95ff9c74853a7dd19d5c94265376d
Bart Bartel <v-babart@microsoft.com>
2021-04-09 13:20:31 -0700

    update uefi notes

--- work/uefi/app/bld.cmd
+++ work/uefi/app/bld.cmd
@@ -13,8 +13,6 @@ REM link.exe /OUT:main.dll /NOLOGO /NODEFAULTLIB /IGNORE:4001 /IGNORE:4254 /OPT:
 
 REM try this one
 "\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\link.exe" /OUT:main.dll /NOLOGO /NODEFAULTLIB /IGNORE:4001 /IGNORE:4281 /IGNORE:4254 /OPT:REF /OPT:ICF=10 /MAP /ALIGN:32 /SECTION:.xdata,D /SECTION:.pdata,D /Machine:X64 /LTCG /DLL /ENTRY:EfiMain /SUBSYSTEM:EFI_BOOT_SERVICE_DRIVER /SAFESEH:NO /BASE:0 /DRIVER /MERGE:.rdata=.data /PDB:main.pdb /PDBALTPATH:main.pdb /ALIGN:4096 main.obj
-
-REM https://edk2-docs.gitbook.io/edk-ii-basetools-user-guides/genfw
 REM 
 C:\git\UEFI-Delos\SM_MU_BASECORE\BaseTools\Bin\Mu-Basetools_extdep\Windows-x86\GenFw.exe -v -e UEFI_APPLICATION main.dll -o main.efi
 
