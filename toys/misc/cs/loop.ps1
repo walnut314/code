f28727af1eb7e0ca1a950e78c332d47bc3f169eb
walnut314 <bartb7@gmail.com>
2021-10-19 12:49:45 -0700

    Update loop.ps1

--- toys/misc/cs/loop.ps1
+++ toys/misc/cs/loop.ps1
@@ -2,6 +2,8 @@
 
 # https://ss64.com/ps/syntax-hash-tables.html
 
+# https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-arrays?view=powershell-7.1
+
 [int[]] $myArray = @()
 
 for ($i=0; $i -lt 4; $i++) {
@@ -14,3 +16,29 @@ Write-Host "length = " + $myArray.length -ForegroundColor Blue
 for ($i=0; $i -lt $myArray.length; $i++) {
     Write-Host "i = " + $myArray[$i] -ForegroundColor Yellow    
 }
+
+Write-Host "iterate"
+foreach($item in $myArray)
+{
+    Write-Output $item
+}
+
+# https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-hashtable?view=powershell-7.1
+#
+# Hash table
+$ageList = @{}
+$key = 'Kevin'
+$value = 36
+$ageList[$key] = $value
+
+$ageList['Claire'] = 9
+$ageList['Taylor'] = 7
+$ageList['Mac'] = 8
+
+
+foreach($key in $ageList.keys)
+{
+    $message = '{0} is {1} years old' -f $key, $ageList[$key]
+    Write-Output $message
+}
+
