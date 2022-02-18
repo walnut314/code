c2cd3836b5cc638d480f6e835ac3c6885d0a7a3b
walnut314 <bartb7@gmail.com>
2022-02-18 15:25:00 -0800

    update eval.py

--- work/dbg/docs/eval.py
+++ work/dbg/docs/eval.py
@@ -14,73 +14,79 @@
 from pykd import *
 import re
 
-
-#print(pykd.dbgCommand("r"))
-#line = print(pykd.dbgCommand("!analyze -v"))
-#def EvalDump():
-num_types = 7
-regex = "Arg(\d): ([a-zA-Z0-9]{16})"
-regex2 = "(\(3D\))|(\(9F\))|(\(A0\))|(\(D1\))|(\(101\))|(\(139\))|(\(1D4\))"
+num_types   = 7 # number of supported bugcheck types
+args_regex  = "Arg(\d): ([a-zA-Z0-9]{16})"
+type_regex  = "(\(3D\))|(\(9F\))|(\(a0\))|(\(D1\))|(\(101\))|(\(139\))|(\(1D4\))"
+grp         = None # this is the dump signature/type, e.g. (9F)
+Args        = [0] * 4 # declare 4-element array for bugcheck arguments
 
 def dumpargs(Args):
     for i in range(len(Args)):
         print("Arg{}: {}".format(i, Args[i]))
 
 def INTERRUPT_EXCEPTION_NOT_HANDLED(Args):
-    print("INTERRUPT_EXCEPTION_NOT_HANDLED")
+    print("INTERRUPT_EXCEPTION_NOT_HANDLED {}".format(grp))
     pass
 def DRIVER_POWER_STATE_FAILURE(Args):
-    print("DRIVER_POWER_STATE_FAILURE")
+    print("DRIVER_POWER_STATE_FAILURE {}".format(grp))
     pass
 def INTERNAL_POWER_ERROR(Args):
-    print("INTERNAL_POWER_ERROR")
+    print("INTERNAL_POWER_ERROR {}".format(grp))
+    print(pykd.dbgCommand("!thread " + Args[3]))
     pass
 def DRIVER_IRQL_NOT_LESS_OR_EQUAL(Args):
-    print("DRIVER_IRQL_NOT_LESS_OR_EQUAL")
+    print("DRIVER_IRQL_NOT_LESS_OR_EQUAL {}".format(grp))
     pass
 def CLOCK_WATCHDOG_TIMEOUT(Args):
-    print("CLOCK_WATCHDOG_TIMEOUT")
+    print("CLOCK_WATCHDOG_TIMEOUT {}".format(grp))
     pass
 def KERNEL_SECURITY_CHECK_FAILURE(Args):
-    print("KERNEL_SECURITY_CHECK_FAILURE")
+    print("KERNEL_SECURITY_CHECK_FAILURE {}".format(grp))
     pass
 def UCMUCSI_LIVEDUMP(Args):
-    print("UCMUCSI_LIVEDUMP")
+    print("UCMUCSI_LIVEDUMP {}".format(grp))
     pass
 
 dump_maps = dict()
 dump_maps["(3D)"]   = ("(3D)",  0x3d,  INTERRUPT_EXCEPTION_NOT_HANDLED);
 dump_maps["(9F)"]   = ("(9F)",  0x9f,  DRIVER_POWER_STATE_FAILURE);
-dump_maps["(A0)"]   = ("(A0)",  0xa0,  INTERNAL_POWER_ERROR);
+dump_maps["(a0)"]   = ("(a0)",  0xa0,  INTERNAL_POWER_ERROR);
 dump_maps["(D1)"]   = ("(D1)",  0xd1,  DRIVER_IRQL_NOT_LESS_OR_EQUAL);
 dump_maps["(101)"]  = ("(101)", 0x101, CLOCK_WATCHDOG_TIMEOUT);
 dump_maps["(139)"]  = ("(139)", 0x139, KERNEL_SECURITY_CHECK_FAILURE);
 dump_maps["(1D4)"]  = ("(1D4)", 0x1d4, UCMUCSI_LIVEDUMP);
 
-def which_group(match):
+def get_dump_type(match):
     for i in range(1,7):
         if match.group(i) != None:
+            print("found")
             return match.group(i)
 
-grp = None
-Args = [1,2,3,4]
-pykd.dbgCommand(".logopen /d")
-print("triage")
-pykd.dbgCommand("||")
+def main():
 
-lines = pykd.dbgCommand("!analyze -v").splitlines()
-for line in lines:
-    if grp == None:
-        typ = re.search(regex2, line)
-        if typ:
-            grp = which_group(typ)
-    arg = re.search(regex, line)
-    if arg:
-        Args[int(arg.group(1))-1] = arg.group(2)
+    global grp
 
-dumpargs(Args)
-dump_maps[grp][2](Args)
+    pykd.dbgCommand(".logopen /d")
+    pykd.dbgCommand("||")
 
-pykd.dbgCommand(".logclose")
-pykd.dbgCommand("$")
+    print("Intel dump analysis")
+    lines = pykd.dbgCommand("!analyze -v").splitlines()
+    for line in lines:
+        if grp == None:
+            typ = re.search(type_regex, line)
+            if typ:
+                grp = get_dump_type(typ)
+        arg = re.search(args_regex, line)
+        if arg:
+            Args[int(arg.group(1))-1] = arg.group(2)
+
+    print("Triaging bugchk {}".format(grp))
+    dumpargs(Args)
+    dump_maps[grp][2](Args)
+
+    pykd.dbgCommand(".logclose")
+    pykd.dbgCommand("$")
+
+if __name__ == '__main__':
+    main()
 
