f426d7d2312d7b6c2f9560ba77710211ae7221f8
walnut314 <bartb7@gmail.com>
2022-02-19 08:11:27 -0800

    wordle

---
+++ toys/misc/wordl/strip.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+# e, t, a, i, o, n, s, h, r
+# beg: t, a, o, d, w
+# end: e, s, d, t
+
+# q w y u o s f j k l z x c v
+# s . i l l 
+
+while (<>) {
+    chomp;
+    if ($_ =~ /(^.....$)/) {
+        print "$1\n";
+    }
+}
+
+# slim down
+while (<>) {
+    chomp;
+    if ($_ =~ /(^.....$)/) {
+        print "$1\n";
+    }
+}
+
