b63ad61b4a29ab0089be39c9de9aae5e25abf8c9
walnut314 <bartb7@gmail.com>
2022-02-24 14:32:58 -0800

    eval pykd

--- work/dbg/docs/eval.py
+++ work/dbg/docs/eval.py
@@ -62,12 +62,60 @@ def get_dump_type(match):
             print("found")
             return match.group(i)
 
+# https://www.stl-tec.de/tutorials/WinReverseEng/pyKD/
+def firstNumberInString(string):
+    # meant to get the middle number out of Windbgs: "Evaluate expression: 1104 = 0000000000000450"
+    return [int(s) for s in string.split() if s.isdigit()][0]
+
+
+def dump_irp_threads():
+    pThreadList = pykd.getOffset("nt!PopIrpThreadList")
+    #pThreadNext = pykd.typedVarList(pThreadList, "nt!_ETHREAD", "ThreadListEntry" )
+    print(hex(pThreadList))
+    #print(pThreadNext)
+
+def dump_procs2():
+    pActiveProcessList = pykd.getOffset("nt!PsActiveProcessHead")
+    #pActiveProcessList = pykd.module("nt").PsActiveProcessHead
+    processList = pykd.typedVarList(pActiveProcessList, "nt!_EPROCESS", "ActiveProcessLinks" )
+    for i, process in enumerate(processList):
+        pykd.dprint("Process " + str(i)+":")
+        name=pykd.loadCStr(process.ImageFileName)
+        print(name)
+
+def dump_procs():
+    pActiveProcessHead = pykd.getOffset("nt!PsActiveProcessHead")
+    currentEntry  = pykd.ptrPtr(pActiveProcessHead)
+    print(hex(pActiveProcessHead))
+    print(hex(currentEntry))
+
+    s = pykd.dbgCommand("? @@(#FIELD_OFFSET(nt!_EPROCESS,ActiveProcessLinks))")
+    offset_eprocess_activeProcesslinks = firstNumberInString(s)
+    #pykd.dprintln("Offset EPROCESS-ActiveProcessLinks: " + str(offset_eprocess_activeProcesslinks))
+    s = pykd.dbgCommand("? @@(#FIELD_OFFSET(nt!_EPROCESS, ImageFileName))")
+    offset_eprocess_imageName = firstNumberInString(s)
+    #pykd.dprintln("Offset EPROCESS-ImageName: " + str(offset_eprocess_imageName))
+
+    while (currentEntry != pActiveProcessHead):
+        #pykd.dprintln("Current Flink:  @ 0x%08x"%currentEntry)
+        pCurrentEProcessObject = currentEntry - offset_eprocess_activeProcesslinks
+        #pykd.dprintln("Current EPROCESS address: @ 0x%08x"%pCurrentEProcessObject)
+        currentImageName = pykd.loadCStr( pCurrentEProcessObject + offset_eprocess_imageName )
+        pykd.dprintln("Current ImageName: "  + currentImageName)
+        if (currentImageName == "csrss.exe"):
+            print("found csrss")
+        currentEntry = pykd.ptrPtr(pCurrentEProcessObject + offset_eprocess_activeProcesslinks)
+    pykd.dprintln("Done")
+
 def main():
 
     global grp
 
     pykd.dbgCommand(".logopen /d")
     pykd.dbgCommand("||")
+
+    dump_irp_threads()
+    return
 
     print("Intel dump analysis")
     lines = pykd.dbgCommand("!analyze -v").splitlines()
