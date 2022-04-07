fdddcfd4cbad9902a95447d124d5452707724014
walnut314 <bart.bartel@intel.com>
2022-04-07 08:09:31 -0700

    Modify dump analysis tools
    Modify dump analysis tools

--- work/dbg/cmds/OKR/anal.ps1
+++ work/dbg/cmds/OKR/anal.ps1
@@ -1,7 +1,9 @@
 # https://www.ervik.as/automating-dump-file-analysis-via-powershell-for-citrix-xenapp/
 
 Set-Alias kd "\Program Files (x86)\Windows Kits\10\Debuggers\x64\kd.exe"
-$DumpFile = "IND220201EV1DK_DripsWatchdog-20220309-0400.dmp"
+#$DumpFile = "dump\IND220201EV1DK_DripsWatchdog-20220309-0400.dmp"
+#$DumpFile = "D:\Intel_Dev\Dumps\BC_9F\MEMORY_DMP\dump\MEMORY.DMP"
+$DumpFile = "D:\Intel_Dev\Dumps\BC_133\MEMORY_CHE221105EV2DK_2022-04-06_00-58-44.DMP"
 $DbgCmds = "`$`$< eval.kd"
 $LogFile = "dump.log"
 kd -c $DbgCmds -noshell -logo $LogFile -z $DumpFile
--- work/dbg/cmds/OKR/eval.py
+++ work/dbg/cmds/OKR/eval.py
@@ -22,7 +22,7 @@ import NOT_IMPLEMENTED as NOT_IMPL
 
 num_types   = 8 # number of supported bugcheck types
 args_regex  = "Arg(\d): ([a-zA-Z0-9]{16})"
-type_regex  = "(\(3D\))|(\(9F\))|(\(a0\))|(\(D1\))|(\(101\))|(\(139\))|(\(1D4\))|(\(15f\))"
+type_regex  = "(3D)|(9F)|(A0)|(D1)|(101)|(139)|(1D4)|(15F)|(133)"
 grp         = None # this is the dump signature/type, e.g. (9F)
 Args        = [0] * 4 # declare 4-element array for bugcheck arguments
 
@@ -30,49 +30,49 @@ def dumpargs(Args):
     for i in range(len(Args)):
         dprintln("Arg{}: {}".format(i, Args[i]))
 
+# Note: the Bugcheck IDs for the hash map need to be UPPERCASE
 dump_maps = dict()
-dump_maps["(15f)"]  = ("(15f)", 0x15f, CS.CONNECTED_STANDBY_WATCHDOG_TIMEOUT_LIVEDUMP);
-dump_maps["(3D)"]   = ("(3D)",  0x3d,  NOT_IMPL.INTERRUPT_EXCEPTION_NOT_HANDLED);
-dump_maps["(9F)"]   = ("(9F)",  0x9f,  NOT_IMPL.DRIVER_POWER_STATE_FAILURE);
-dump_maps["(a0)"]   = ("(a0)",  0xa0,  NOT_IMPL.INTERNAL_POWER_ERROR);
-dump_maps["(D1)"]   = ("(D1)",  0xd1,  NOT_IMPL.DRIVER_IRQL_NOT_LESS_OR_EQUAL);
-dump_maps["(101)"]  = ("(101)", 0x101, NOT_IMPL.CLOCK_WATCHDOG_TIMEOUT);
-dump_maps["(139)"]  = ("(139)", 0x139, NOT_IMPL.KERNEL_SECURITY_CHECK_FAILURE);
-dump_maps["(1D4)"]  = ("(1D4)", 0x1d4, NOT_IMPL.UCMUCSI_LIVEDUMP);
+dump_maps["3D"]   = NOT_IMPL.INTERRUPT_EXCEPTION_NOT_HANDLED;
+dump_maps["9F"]   = NOT_IMPL.DRIVER_POWER_STATE_FAILURE;
+dump_maps["A0"]   = NOT_IMPL.INTERNAL_POWER_ERROR;
+dump_maps["D1"]   = NOT_IMPL.DRIVER_IRQL_NOT_LESS_OR_EQUAL;
+dump_maps["101"]  = NOT_IMPL.CLOCK_WATCHDOG_TIMEOUT;
+dump_maps["133"]  = NOT_IMPL.DPC_WATCHDOG_VIOLATION;
+dump_maps["139"]  = NOT_IMPL.KERNEL_SECURITY_CHECK_FAILURE;
+dump_maps["1D4"]  = NOT_IMPL.UCMUCSI_LIVEDUMP;
+dump_maps["15F"]  = CS.CONNECTED_STANDBY_WATCHDOG_TIMEOUT_LIVEDUMP;
 
 def get_dump_type(match):
     global num_types
     dprintln("dump match {}".format(match))
-    #for i in range(0,num_types):
-#        dprintln("grp {}={}".format(i, match.group(i)))
+    for i in range(0,num_types):
+        dprintln("grp {}={}".format(i, match.group(i)))
     if match.group(0) != None:
-        dprintln("found")
+        dprintln("found {}".format(match.group(0)))
         return match.group(0)
     dprintln("no match found")
 
 def main():
-
     global grp
-
-    #pykd.dbgCommand(".logopen /d")
-    #pykd.dbgCommand("||")
-    #dump_irp_threads()
-    #return
 
     dprint(': {}' .format(float(0.27)) + ' Gbps\n')
     dprint("Intel dump analysis\n")
     lines = pykd.dbgCommand("!analyze -v").splitlines()
     for line in lines:
         if grp == None:
-            typ = re.search(type_regex, line)
+            typ = re.search(type_regex, line, re.IGNORECASE)
             if typ:
+                print("type {}".format(typ))
                 grp = get_dump_type(typ)
         arg = re.search(args_regex, line)
         if arg:
-            Args[int(arg.group(1))-1] = arg.group(2)
+            print(arg)
+            Args[int(arg.group(1))-1] = arg.group(0) #(2)
 
-    dprintln("Triaging bugchk {}".format(grp))
-    dump_maps[grp][2](Args) ## call bugcode handler
+    if grp != None:
+        grp = grp.upper()
+        dprintln("Triaging bugchk {}".format(grp))
+        dump_maps[grp](Args) ## call bugcode handler
 
     pykd.dbgCommand(".logclose")
     pykd.dbgCommand("$")
