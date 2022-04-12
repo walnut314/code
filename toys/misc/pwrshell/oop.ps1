c03ee4decd30fa832e8da4131b8eb92c43fb002c
walnut314 <bart.bartel@intel.com>
2022-04-12 14:59:46 -0700

    powershell update

--- toys/misc/pwrshell/oop.ps1
+++ toys/misc/pwrshell/oop.ps1
@@ -1,38 +1,5 @@
-class Device {
-    [string]$Brand
-    [string]$Model
-    [string]$VendorSku
-
-    [string]ToString(){
-        return ("{0}|{1}|{2}" -f $this.Brand, $this.Model, $this.VendorSku)
-    }
-}
-
-class Rack {
-    [int]$Slots = 8
-    [string]$Brand
-    [string]$Model
-    [string]$VendorSku
-    [string]$AssetId
-    [Device[]]$Devices = [Device[]]::new($this.Slots)
-    [void] AddDevice([Device]$dev, [int]$slot){
-        $this.Devices[$slot] = $dev
-    }
-    [void]RemoveDevice([int]$slot){
-        $this.Devices[$slot] = $null
-    }
-    [int[]] GetAvailableSlots(){
-        [int]$i = 0
-        return @($this.Devices.foreach{ if($_ -eq $null){$i}; $i++})
-    }
-    [void] DumpSlots(){
-        for ($i=0; $i -lt $this.Slots; $i++){ #$dev in $this.Devices){
-            $myString = "'$($i)' is '$($this.Devices[$i])'"
-#https://hodgkins.io/the-ultimate-guide-to-powershell-string-formatting                
-            Write-Host $myString # Write-Output
-        }
-    }
-}
+# include header
+. .\dev.ps1
 
 $rack = [Rack]::new()
 $surface = [Device]::new()
