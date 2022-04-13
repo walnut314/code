71168abcbc82ebeb6364149b54748e320087211c
walnut314 <bart.bartel@intel.com>
2022-04-13 15:04:22 -0700

    anal.ps1 for OKR modified

--- work/dbg/cmds/OKR/anal.ps1
+++ work/dbg/cmds/OKR/anal.ps1
@@ -1,14 +1,32 @@
+# https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/kd-command-line-options
 
-Set-Alias kd "\Program Files (x86)\Windows Kits\10\Debuggers\x64\kd.exe"
+if ($env:_NT_KD_PATH) {
+    Set-Alias kd $env:_NT_KD_PATH
+} else {
+    Set-Alias kd "\Program Files (x86)\Windows Kits\10\Debuggers\x64\kd.exe"
+}
+
+#write-host "There are a total of $($args.count) arguments"
+#for ( $i = 0; $i -lt $args.count; $i++ ) {
+#    write-host "Argument  $i is $($args[$i])"
+#} 
+
+if ($args.count -eq 0) {
 #$DumpFile = "dump\IND220201EV1DK_DripsWatchdog-20220309-0400.dmp"
 #$DumpFile = "D:\Intel_Dev\Dumps\BC_9F\MEMORY_DMP\dump\MEMORY.DMP"
-$DumpFile = "D:\Intel_Dev\Dumps\BC_133\MEMORY_CHE221105EV2DK_2022-04-06_00-58-44.DMP"
+#$DumpFile = "D:\Intel_Dev\Dumps\BC_133\MEMORY_CHE221105EV2DK_2022-04-06_00-58-44.DMP"
+#$DumpFile = "D:\Intel_Dev\Dumps\BC_15F\IND220201EV1DK_DripsWatchdog-20220309-0400\IND220201EV1DK_DripsWatchdog-20220309-0400.dmp"
+    $DumpFile = "D:\Intel_Dev\Dumps\BC_15F\IND220201EV1DK_DripsWatchdog-20220309-0400\IND220201EV1DK_DripsWatchdog-20220309-0400.dmp"
+    Write-Host "you didn't provide a dump, so I'm using: $($DumpFile)"
+    #Exit
+} else {
+    $DumpFile = $args[0];
+}
+
 $DbgCmds = "`$`$< eval.kd"
 $LogFile = "analysis.log"
 
-# https://docs.microsoft.com/en-us/windows-hardware/drivers/debugger/kd-command-line-options
 #kd -c $DbgCmds -noshell -loga $LogFile -z $DumpFile
-
 kd -c $DbgCmds -noshell -z $DumpFile
 
 
