ddd91a38024391a66aaf4e7c201d29ac6dbdc56f
walnut314 <bart.bartel@intel.com>
2022-05-06 13:03:40 -0700

    modify shell inits for dynamic drive letter

--- work/init_me.cmd
+++ work/init_me.cmd
@@ -1,9 +1,13 @@
 @echo off
 
 setlocal enabledelayedexpansion
-rem title=authorized hacker
+rem title=intel
 
 rem Create vector with names of days
+
+if %1.==. goto Exit
+set DriveLetter=%1
+echo %DriveLetter%
 
 set i=0
 for %%d in (Sunday Monday Tuesday Wednesday Thrusday Friday Saturday) do (
@@ -25,7 +29,7 @@ set quotes[7]=insecure
 set /a num=%random% %%7
 echo %num%
 REM echo !quotes[%num%]!
-title !quotes[%num%]!
+rem title !quotes[%num%]!
 
 set /a even=%num% %%2
 rem if /i "%even%"=="1" (
@@ -37,43 +41,31 @@ color 02
 
 rem symbol paths
 set DUDE=dude
-set _NT_SYMBOL_PATH="srv*;C:\git\SurfaceOemPanelDriver\Debug\x64\bin;C:\git\SurfaceTconDriver\Debug\x64\bin"
-set _NT_SOURCE_PATH="C:\git\SurfaceTconDriver\sys;C:\git\SurfaceLibrary\Kernel\DMF"
+rem set _NT_SYMBOL_PATH="srv*;C:\git\SurfaceOemPanelDriver\Debug\x64\bin;C:\git\SurfaceTconDriver\Debug\x64\bin"
+rem set _NT_SOURCE_PATH="C:\git\SurfaceTconDriver\sys;C:\git\SurfaceLibrary\Kernel\DMF"
 
 doskey ise=powershell_ise
-doskey dev=cd /d %SystemDrive%\git
-doskey rtos=cd /d %SystemDrive%\sandbox\rtos\FreeRTOSv10.3.1\FreeRTOSv10.3.1\FreeRTOS\Demo\WIN32-MSVC
-doskey jaz=cd /d %SystemDrive%\git\Jaz-SAM
-doskey zap=cd /d %SystemDrive%\git\mcu
-doskey skip=cd /d %SystemDrive%\git\mcu\src\projects\zap\skip-b
-doskey sis=cd /d %SystemDrive%\git\SurfaceService
-doskey uefi=cd /d %SystemDrive%\git\uefi-delos
-doskey avila=cd /d %SystemDrive%\git\avila-sam
-doskey paloma=cd /d %SystemDrive%\git\paloma-sam
-doskey haven=cd /d %SystemDrive%\git\havenu-sam
-doskey cambria=cd /d %SystemDrive%\git\cambria-sam
-doskey cruz=cd /d %SystemDrive%\git\cruz-sam
-doskey fox=cd /d %SystemDrive%\git\foxburg-sam
-doskey jupiter=cd /d %SystemDrive%\git\mcu-ple
-doskey gas=cd /d %SystemDrive%\git\GasGauge\SP4_GasGaugeTelemetryDecoder
-doskey sand=cd /d %SystemDrive%\sandbox
-doskey tools=cd /d %SystemDrive%\sandbox\tools
-doskey os=cd /d %SystemDrive%\sandbox\reactos
+doskey dev=cd /d %SystemDrive%\dev
+doskey sand=cd /d c:\sandbox
+doskey lily=cd /d c:\sandbox ^&^& cd ^&^& lilypad
+doskey sym=cd /d c:\symbols
+doskey tools=cd /d c:\sandbox\tools
+doskey wdk=cd /d c:\sandbox\wdk
+doskey moltools=cd /d "C:\Users\bbartel\OneDrive - Intel Corporation\Documents\WindowsPowerShell\Modules\MOLTools"
 doskey dmf=cd /d %SystemDrive%\git\SurfaceLibrary\Kernel\DMF
-doskey tcon=cd /d %SystemDrive%\git\SurfaceTconDriver
-doskey oem=cd /d %SystemDrive%\git\SurfaceOemPanelDriver
-doskey smf=cd /d %SystemDrive%\git\SurfaceSystemManagementFrameworkDriver
-doskey sid=cd /d %SystemDrive%\git\SurfaceIntegrationDriver
-doskey slib=cd /d %SystemDrive%\git\SurfaceLibrary
-doskey ser=cd /d %SystemDrive%\git\SurfaceSerialHubDriver
-doskey grok=cd /d C:\sandbox\code\toys\grokkage
-doskey webapp=cd /d C:\sandbox\code\toys\misc\webapp
+doskey grok=cd /d C:\dev\code\toys\grokkage
 doskey inc=cd /d "C:\Program Files (x86)\Windows Kits\10\Include"
-doskey fin=cd /d C:\sandbox\fin
-doskey work=cd /d C:\sandbox\code\work
-doskey bug=cd /d C:\sandbox\dumps
-doskey tfs=cd /d C:\TFS\PLE\Users\v-babart
-doskey dbg=cd /d C:\sandbox\code\work\dbg\docs
+doskey work=cd /d C:\dev\code\work
+doskey dump=cd /d %DriveLetter%\Intel_Dev\Dumps
+doskey largo=cd /d %DriveLetter%\Intel_Dev\Bugs\Largo
+doskey ffu=cd /d %DriveLetter%\Intel_Dev\Bugs\Largo\FFU_Image
+doskey charts=cd /d %DriveLetter%\Intel_Dev\Bugs\Charts
+doskey bug=cd /d %DriveLetter%\Intel_Dev\Bugs
+doskey pst=cd /d C:\dev\AutoPST
+doskey dbg=cd /d C:\dev\code\work\dbg\docs
+doskey wordl=cd /d C:\dev\code\toys\misc\wordl
+doskey okr=cd /d C:\dev\Code\work\dbg\cmds\OKR
+doskey prj=cd /d C:\dev\code\toys\cbox\algo\PRJ
 doskey home=cd /d %USERPROFILE%
 doskey green=color 02
 doskey yellow=color 06
@@ -86,7 +78,10 @@ doskey cp=xcopy $*
 doskey mv=move $*
 doskey rm=del $*
 doskey grep=findstr $*
+doskey ddk=C:\WinDDK\7600.16385.1\bin\setenv C:\WinDDK\7600.16385.1 chk
 doskey alias=doskey /macros
 
 @echo on
+
+:Exit
 
--- work/init_me.ps1
+++ work/init_me.ps1
@@ -4,6 +4,15 @@
 #{
 #    Write-Host "$($profileLocation): $($PROFILE.$profileLocation)"
 #}
+
+$DriveLetter = "F:"
+if ($args.count -eq 0) {
+    Write-Host "you didn't provide a drive letter, so I'm using: $($DumpFile)"
+} else {
+    $DriveLetter = $args[0];
+}
+Write-Host "using drive letter: " $DriveLetter
+
 $a = Get-Module PSReadLine
 if ($a) {
     Remove-Module PSReadLine
@@ -26,15 +35,17 @@ doskey /exename=powershell.exe wdk=Set-Location -Path "c:\sandbox\wdk"
 doskey /exename=powershell.exe grok=Set-Location -Path "C:\dev\code\toys\grokkage"
 doskey /exename=powershell.exe inc=Set-Location -Path "C:\Program Files (x86)\Windows Kits\10\Include"
 doskey /exename=powershell.exe work=Set-Location -Path "C:\dev\code\work"
-doskey /exename=powershell.exe dump=Set-Location -Path "D:\Intel_Dev\Dumps"
-doskey /exename=powershell.exe largo=Set-Location -Path "D:\Intel_Dev\Bugs\Largo"
-doskey /exename=powershell.exe ffu=Set-Location -Path "D:\Intel_Dev\Bugs\Largo\FFU_Image"
-doskey /exename=powershell.exe charts=Set-Location -Path "D:\Intel_Dev\Bugs\Charts"
-doskey /exename=powershell.exe bug=Set-Location -Path "D:\Intel_Dev\Bugs"
+doskey /exename=powershell.exe dump=Set-Location -Path $DriveLetter"\Intel_Dev\Dumps"
+doskey /exename=powershell.exe largo=Set-Location -Path $DriveLetter"\Intel_Dev\Bugs\Largo"
+doskey /exename=powershell.exe ffu=Set-Location -Path $DriveLetter"\Intel_Dev\Bugs\Largo\FFU_Image"
+doskey /exename=powershell.exe charts=Set-Location -Path $DriveLetter"\Intel_Dev\Bugs\Charts"
+doskey /exename=powershell.exe bug=Set-Location -Path $DriveLetter"\Intel_Dev\Bugs"
 doskey /exename=powershell.exe pst=Set-Location -Path "C:\dev\AutoPST"
 doskey /exename=powershell.exe dbg=Set-Location -Path "C:\dev\code\work\dbg\docs"
 doskey /exename=powershell.exe wordl=Set-Location -Path "C:\dev\code\toys\misc\wordl"
 doskey /exename=powershell.exe okr=Set-Location -Path "C:\dev\Code\work\dbg\cmds\OKR"
+doskey /exename=powershell.exe prj=Set-Location -Path "C:\dev\code\toys\cbox\algo\PRJ"
 doskey /exename=powershell.exe home=Set-Location -Path "$env:USERPROFILE"
+doskey /exename=powershell.exe macs=doskey /macros:all
 doskey /exename=powershell.exe pur=del *~
 
