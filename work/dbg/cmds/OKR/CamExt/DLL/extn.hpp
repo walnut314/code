95b0cd661b7772871da126b7e3f30adf0375642a
Bart Bartel <bart.bartel@intel.com>
2023-07-15 03:57:37 -0700

    cleanup kd ext

--- work/dbg/cmds/OKR/CamExt/DLL/extn.hpp
+++ work/dbg/cmds/OKR/CamExt/DLL/extn.hpp
@@ -14,13 +14,15 @@ public:
         }
         m_DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Extension:ctor\n");
     }
-    ~Extension() {
+    ~Extension()
+    {
         m_DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Extension:dtor\n");
         ReleaseInterfaces();
     }
 
     HRESULT Exec(
-            _In_ PCHAR cmd) {
+            _In_ PCHAR cmd)
+    {
         HRESULT Status = S_OK;
         Status = m_DebugControl->Execute(DEBUG_OUTCTL_ALL_CLIENTS, cmd, DEBUG_EXECUTE_ECHO);
         return Status;
@@ -28,20 +30,14 @@ public:
 
     HRESULT Eval(
             _In_ PCHAR expr,
-            _Inout_ PDEBUG_VALUE val) {
-
+            _Inout_ PDEBUG_VALUE val)
+    {
         HRESULT Status = S_OK;
 
         __try {
-            if ((Status = m_DebugControl->SetExpressionSyntax(DEBUG_EXPR_CPLUSPLUS)) != S_OK) {
-                return Status;
-            }
-            if ((Status = m_DebugControl->Evaluate(expr, DEBUG_VALUE_INVALID, val, NULL)) != S_OK) {
-                return Status;
-            }
-            if ((Status = m_DebugControl->SetExpressionSyntax(DEBUG_EXPR_MASM)) != S_OK) {
-                return Status;
-            }
+            if ((Status = m_DebugControl->SetExpressionSyntax(DEBUG_EXPR_CPLUSPLUS))      != S_OK) { return Status; }
+            if ((Status = m_DebugControl->Evaluate(expr, DEBUG_VALUE_INVALID, val, NULL)) != S_OK) { return Status; }
+            if ((Status = m_DebugControl->SetExpressionSyntax(DEBUG_EXPR_MASM))           != S_OK) { return Status; }
             m_DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Val: 0x%I64x\n", val->I64);
         }
 
@@ -53,7 +49,8 @@ public:
     HRESULT
     Out(
         _In_ PCHAR str
-       ) {
+       )
+    {
         m_DebugControl->Output(DEBUG_OUTPUT_NORMAL, str);
     }
 
@@ -62,15 +59,9 @@ public:
         _In_ PDEBUG_CLIENT DebugClient
         ) {
         HRESULT Status = S_OK;
-        if ((Status = DebugClient->QueryInterface(__uuidof(IDebugControl), (PVOID *)&m_DebugControl)) != S_OK) {
-            return Status;
-        }
-        if ((Status = DebugClient->QueryInterface(__uuidof(IDebugSymbols), (PVOID *)&m_DebugSymbols)) != S_OK) {
-            return Status;
-        }
-        if ((Status = DebugClient->QueryInterface(__uuidof(IDebugDataSpaces), (PVOID *)&m_DebugDataSpaces)) != S_OK) {
-            return Status;
-        }
+        if ((Status = DebugClient->QueryInterface(__uuidof(IDebugControl),      (PVOID *)&m_DebugControl))    != S_OK) { return Status; }
+        if ((Status = DebugClient->QueryInterface(__uuidof(IDebugSymbols),      (PVOID *)&m_DebugSymbols))    != S_OK) { return Status; }
+        if ((Status = DebugClient->QueryInterface(__uuidof(IDebugDataSpaces),   (PVOID *)&m_DebugDataSpaces)) != S_OK) { return Status; }
         return Status;
     }
 
@@ -79,18 +70,9 @@ public:
         VOID
         )
     {
-        if (m_DebugDataSpaces) {
-            m_DebugDataSpaces->Release();
-            m_DebugDataSpaces = NULL;
-        }
-        if (m_DebugSymbols) {
-            m_DebugSymbols->Release();
-            m_DebugSymbols = NULL;
-        }
-        if (m_DebugControl) {
-            m_DebugControl->Release();
-            m_DebugControl = NULL;
-        }
+        if (m_DebugDataSpaces)  { m_DebugDataSpaces->Release(); m_DebugDataSpaces   = NULL; }
+        if (m_DebugSymbols)     { m_DebugSymbols->Release();    m_DebugSymbols      = NULL; }
+        if (m_DebugControl)     { m_DebugControl->Release();    m_DebugControl      = NULL; }
     }
 
 private:
