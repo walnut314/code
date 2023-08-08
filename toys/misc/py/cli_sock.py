1baac746fa6f7429513df4b3a80669823517ba80
Bart Bartel <bart.bartel@intel.com>
2023-08-08 10:48:42 -0700

    update cli/srv socks

--- toys/misc/py/cli_sock.py
+++ toys/misc/py/cli_sock.py
@@ -4,13 +4,14 @@ HOST = 'localhost'
 PORT = 9876
 ADDR = (HOST,PORT)
 BUFSIZE = 4096
-#videofile = "data.txt"
+videofile = "notes.txt"
 client = 0
 
 import fileinput
 
 while True:
     for line in fileinput.input():
+        print(line)
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(ADDR)
         client.send(line)
