abfad3631ba205ab4f17e96c955159fd83e59652
Bart Bartel <bart.bartel@intel.com>
2023-09-11 11:39:52 -0700

    update camera debug extension

--- work/dbg/cmds/OKR/KdCam/camext.c
+++ work/dbg/cmds/OKR/KdCam/camext.c
@@ -1,62 +1,4 @@
-#include <ntddk.h>
-#include <windef.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define LMEM_FIXED          0x0000
-#define LMEM_MOVEABLE       0x0002
-#define LMEM_NOCOMPACT      0x0010
-#define LMEM_NODISCARD      0x0020
-#define LMEM_ZEROINIT       0x0040
-#define LMEM_MODIFY         0x0080
-#define LMEM_DISCARDABLE    0x0F00
-#define LMEM_VALID_FLAGS    0x0F72
-#define LMEM_INVALID_HANDLE 0x8000
-
-#define LPTR (LMEM_FIXED | LMEM_ZEROINIT)
-
-#define WINBASEAPI
-    
-WINBASEAPI
-HLOCAL
-WINAPI
-LocalAlloc(
-    UINT uFlags,
-    UINT uBytes
-    );
-
-WINBASEAPI
-HLOCAL
-WINAPI
-LocalFree(
-    HLOCAL hMem
-    );
-
-#define CopyMemory RtlCopyMemory
-#define FillMemory RtlFillMemory
-#define ZeroMemory RtlZeroMemory
-
-//
-// Now we can bring in the WINDBG extension
-// definitions...
-//
-#include <wdbgexts.h>   // located in MSTOOLS\H
-
-#include <shared_memory_access.h>
-#include <shared_memory_access_priv.h>
-#include <memory_range.h>
-
-static EXT_API_VERSION
-    ApiVersion = { 5, 1, EXT_API_VERSION_NUMBER64, 0 };
-
-static WINDBG_EXTENSION_APIS ExtensionApis;
-
-static USHORT SavedMajorVersion;
-static USHORT SavedMinorVersion;
-
-static USHORT CameraVersion = 13489;
+#include "camext.h"
 
 LPEXT_API_VERSION
 ExtensionApiVersion(
@@ -71,9 +13,6 @@ CheckVersion(
     VOID
     )
 {
-    //
-    // Your version-checking code goes here
-    //
     dprintf( 
         "CheckVersion called... [%1x;%d]\n",
         SavedMajorVersion,
@@ -122,7 +61,18 @@ WinDbgExtensionDllInit(
 
 DECLARE_API(help)
 {
-    dprintf("mem -- dumps the used_mem_list\n");
+    dprintf("\nCopyright © 2023 Intel Corp. All rights reserved.\n\n");
+    dprintf("help       - Displays this list.\n");
+    dprintf("mem        - Dumps the used_mem_list\n");
+}
+
+DECLARE_API( version )
+{
+    dprintf( "Camera extension dll for Camera Build %d debugging %s kernel for Kernel Build %d\n",
+             CameraVersion,
+             SavedMajorVersion == 0x0c ? "Checked" : "Free",
+             SavedMinorVersion
+           );
 }
 
 DECLARE_API(mem)
@@ -162,13 +112,54 @@ DECLARE_API(mem)
 
 }
 
-DECLARE_API( version )
+//#include <framecontext.h>
+//#include <isp_frame.h>
+DECLARE_API(frames)
 {
-    dprintf( "Camera extension dll for Camera Build %d debugging %s kernel for Kernel Build %d\n",
-             CameraVersion,
-             SavedMajorVersion == 0x0c ? "Checked" : "Free",
-             SavedMinorVersion
-           );
+    ULONG64 Count;
+    ULONG64 Frames;
+    ULONG32 n_Frames;
+    int Size;
+    DWORD dwBytesRead;
+    //_DrvFrameContext frame_ctx;
+    int i;
+
+    Count  = GetExpression("iacamera64!g_MaxFramesToBeQueued");
+    Frames = GetExpression("iacamera64!g_frameContexts");
+    //Size   = GetExpression("?? sizeof(iacamera64!_DrvFrameContext)");
+    //Size   = GetExpression("sizeof(iacamera64!_DrvFrameContext)");
+    Size   = 0xdeadbeef; // Exec("sizeof(iacamera64!g_frameContexts)");
+    dprintf("Count: %I64x, Frames: %I64x, Size: %I64d\n", Count, Frames, Size);
+
+    ReadMemory(Count, &n_Frames, sizeof( ULONG32 ), &dwBytesRead);
+    dprintf("num frames: %d\n", n_Frames);
+    //size = sizeof(_DrvFrameContext);
+    for (i = 0; i < n_Frames; i++) {
+
+    }
+#if 0
+    dprintf("n frames %d\n", n_Frames);
+
+    if( !ReadMemory(Address, &list, sizeof( LIST_ENTRY ), &dwBytesRead))
+    {
+        dprintf( "Can't get list.\n ");
+        return;
+    }
+
+    dprintf("flink: %I64x\n", list.Flink);
+    Flink = (ULONG64) list.Flink;
+    Blink = (ULONG64) list.Blink;
+
+    while (Flink != Blink) {
+        ReadMemory(Flink, &mem, sizeof(memory_range_t), &dwBytesRead);
+        dprintf("host addr %I64x\n", mem.host_addr);
+        dprintf("mdl       %I64x\n", mem.page_addr);
+        dprintf("size      0n%d\n", mem.actual_size);
+        total_size += mem.actual_size;
+        Flink = (ULONG64) mem.list.Flink;
+    }
+    dprintf("total size 0n%d bytes, 0n%d meg \n", total_size, total_size/1024/1024);
+#endif
 }
 
 
