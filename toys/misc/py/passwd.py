2be693b63c9dcaef1fe613ddb412a66f46b1d023
Bart Bartel <bartb7@gmail.com>
2023-12-10 13:00:31 -0800

    mod'd passwd.py to use string.ascii_letters

--- toys/misc/py/passwd.py
+++ toys/misc/py/passwd.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import random, string
 length = 12
-print "automatic password generator: length = ", length
+print("automatic password generator: length = ", length)
 myrg = random.SystemRandom()
 # if you want non-English characters, remove the [0:52]
 #alphabet = string.letters[0:52] + string.digits
-alphabet = string.letters + string.digits + '$' + '^' + '@' + '%'
+alphabet = string.ascii_letters + string.digits + '$' + '^' + '@' + '%'
 pw = str().join(myrg.choice(alphabet)
 for _ in range(length))
-print "password is: ", pw
+print("password is: ", pw)
 
