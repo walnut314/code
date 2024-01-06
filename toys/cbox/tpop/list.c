7b0a1e648788b5c4a549b86545f17cc19fef04b2
Bart Bartel <bartb7@gmail.com>
2024-01-06 10:55:13 -0800

    add template tpop list

---
+++ toys/cbox/tpop/list.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _Nameval {
+    char *name;
+    int  value;
+    struct _Nameval *next;
+} Nameval;
+
+int main()
+{
+}
+
