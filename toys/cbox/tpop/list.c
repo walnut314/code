1c96b21c00d168aa5a659fb78c3ea4bda04a3dcd
Bart Bartel <bartb7@gmail.com>
2024-01-06 14:48:44 -0800

    list update

--- toys/cbox/tpop/list.c
+++ toys/cbox/tpop/list.c
@@ -8,7 +8,66 @@ typedef struct _Nameval {
     struct _Nameval *next;
 } Nameval;
 
+Nameval *newitem(char *name, int value)
+{
+    Nameval *newp;
+    newp = (Nameval *) malloc(sizeof(Nameval));
+    newp->name = name;
+    newp->value = value;
+    newp->next = NULL;
+    return newp;
+}
+
+Nameval *addfront(Nameval *listp, Nameval *newp)
+{
+    newp->next = listp->next;
+    listp->next = newp;
+    return newp;
+}
+
+Nameval *addend(Nameval *listp, Nameval *newp)
+{
+    Nameval *p;
+    if (listp == NULL)
+        return newp;
+    for (p = listp; p->next != NULL; p = p->next)
+        ;
+    p->next = newp;
+    return listp;
+}
+
+Nameval *lookup(Nameval *listp, char *name)
+{
+    for ( ; listp != NULL; listp = listp->next)
+        if (strcmp(name, listp->name) == 0)
+            return listp;
+    return NULL;
+}
+
+void apply(Nameval *listp, void (*fn)(Nameval*, void*), void *arg)
+{
+    for ( ; listp != NULL; listp = listp->next)
+        (*fn)(listp, arg);
+}
+
+void printnv(Nameval *p, void *arg)
+{
+    char *fmt;
+    fmt = (char *) arg;
+    printf(fmt, p->name, p->value);
+}
+
+
 int main()
 {
+    char *nms[] = { "fred", "wilma", "barney", "betty" };
+
+    Nameval *nvlist = newitem("", 0xdeadbeef);
+    Nameval *nval;
+    for (int i = 0; i < 4; i++) {
+        nval = newitem(nms[i], i);
+        addfront(nvlist, nval);
+    }
+    apply(nvlist, printnv, "dude: %s: %x\n");
 }
 
