fb569223eac41139ccbcbb132ced41092cfe1847
Bart Bartel <bartb7@gmail.com>
2024-02-06 14:58:33 -0800

    code and HOA stuff

---
+++ toys/cbox/tpop/csv.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char buf[200];
+char *field[20];
+
+char *unquote(char *p)
+{
+    if (p[0] == '"') {
+        if (p[strlen(p)-1] == '"')
+            p[strlen(p)-1] = '\0';
+        p++;
+    }
+    return p;
+}
+
+int csvgetline(FILE *fin)
+{
+    int nfield;
+    char *p, *q;
+
+    if (fgets(buf, sizeof(buf), fin) == NULL)
+        return -1;
+    nfield = 0;
+    for (q = buf; (p=strtok(q, ",\n\r")) != NULL; q = NULL)
+        field[nfield++] = unquote(p);
+    return nfield;
+}
+
+int main()
+{
+    int i, nf;
+
+    while ((nf = csvgetline(stdin)) != -1)
+        for (i = 0; i < nf; i++)
+            printf("field[%d] = '%s'\n", i, field[i]);
+    return 0;
+}
+
