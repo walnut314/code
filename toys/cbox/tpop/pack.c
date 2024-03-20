444b2d40ecfd74c0800ce21c44ebb42115af6b59
Bart Bartel <bartb7@gmail.com>
2024-03-20 00:32:16 -0700

    pack

--- toys/cbox/tpop/pack.c
+++ toys/cbox/tpop/pack.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef unsigned char   uchar;
 typedef unsigned short  ushort;
@@ -86,9 +88,26 @@ int pack_type1(uchar *buf, ushort count, uchar val, ulong data)
     return pack(buf, "cscl", 0x01, count, val, data);
 }
 
-
+int unpack_type1(int n, uchar *buf)
+{
+    uchar c1, c2;
+    ushort count;
+    ulong dw;
+    if (unpack(buf, "cscl", &c1, &count, &c2, &dw) != n)
+        return -1;
+    printf("c1: %c, count: %d, c2: %c, data: %lx\n", c1, count, c2, dw);
+    return n;
+}
 
 int main()
 {
+    uchar buf[256];
+    int ret;
+    memset(buf, 0, 256);
+    ret = pack_type1(buf, 0x03, 'a', 0xdeadbeef);
+    printf("ret: %d\n", ret);
+
+    ret = unpack_type1(8, buf);
+    printf("ret: %d\n", ret);
 }
 
