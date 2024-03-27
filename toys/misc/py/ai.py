6ec09c9bbf2d64616e08b3da18d002dcb776b7bb
Bart Bartel <bartb7@gmail.com>
2024-03-26 23:28:14 -0700

    python path update

--- toys/misc/py/ai.py
+++ toys/misc/py/ai.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # https://medium.com/technology-invention-and-more/how-to-build-a-simple-neural-network-in-9-lines-of-python-code-cc8f23647ca1
 # http://iamtrask.github.io/2015/07/12/basic-python-network/
@@ -12,5 +12,5 @@ synaptic_weights = 2 * random.random((3, 1)) - 1
 for iteration in xrange(10000):
     output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
-print 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights))))
+print(1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights)))))
 
