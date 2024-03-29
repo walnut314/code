95ba67b97e309b9592a32161675159a511995f0a
Bart Bartel <bartb7@gmail.com>
2024-03-29 10:05:40 -0700

    update cheater

--- toys/cbox/tpop/cheat.c
+++ toys/cbox/tpop/cheat.c
@@ -10,8 +10,25 @@ int count_bits(int x)
     return count;
 }
 
+#define exchg(a, b) { int t = a; a = b; b = t; }
+void sort(int a[], int n)
+{
+    for (int i = 1; i <= n-1; i++) {
+        int d = i;
+        while (d > 0 && a[d] < a[d-1]) { exchg(a[d], a[d-1]); d--; }
+    }
+}
+
+void show(int a[], int n) {
+    for (int i = 0; i < n; i++) { printf("%d) %d\n", i, a[i]); } printf("\n");
+}
 
 int main()
 {
+    int a[] = {2,6,4,9,1,3,2};
     printf("count bits: %d\n", count_bits(0x1713));
+    show(a, 7);
+    sort(a, 7);
+    show(a, 7);
+
 }
