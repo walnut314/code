614f19e9c92a4b65dee1943915bd8ffaec656002
Bart Bartel <bartb7@gmail.com>
2024-06-17 08:54:32 -0700

    do QT

--- toys/cbox/tpop/crack/qt.c
+++ toys/cbox/tpop/crack/qt.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+int count_bits(int val)
+{
+    int count = 0;
+    while (val) {
+        count++;
+        val &= (val-1);
+    }
+    return count;
+}
+
 
 
 
@@ -8,4 +18,6 @@ int main(int argc, char *argv[])
     for (int i = 0; i < argc; i++) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
+    int val = 0x3013;
+    printf("there are %d bits in %x\n", count_bits(val), val);
 }
