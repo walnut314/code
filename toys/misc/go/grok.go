1f64fd667a6ffa7df430bae17a366e4f5dc855e0
Bart Bartel <bartb7@gmail.com>
2024-07-30 07:54:40 -0700

    update grokkage

--- toys/misc/go/grok.go
+++ toys/misc/go/grok.go
@@ -4,6 +4,33 @@ import (
     "fmt"
 )
 
+func sum(a []int) int {
+    if len(a) == 0 {
+        return 0
+    } else {
+        return a[0] + sum(a[1:])
+    }
+}
+
+func count(a []int) int {
+    if len(a) == 0 {
+        return 0
+    } else {
+        return 1 + count(a[1:])
+    }
+}
+
+func maxi(a []int) int {
+    if len(a) == 1 {
+        return a[0]
+    }
+    max := maxi(a[:len(a)-1])
+    if a[len(a)-1] > max {
+      return a[len(a)-1]
+   }
+   return max
+}
+
 func binsea(a []int, key int) int {
     lo := 0
     hi := len(a)-1
@@ -44,6 +71,28 @@ func selsort(a []int) []int {
     return newarr
 }
 
+func qsort(a []int) []int {
+    if len(a) < 2 {
+        return a
+    } else {
+        pivot   := a[0]
+        less    := []int{}
+        greater := []int{}
+        for i := 1; i < len(a); i++ {
+            if a[i] <= pivot {
+                less = append(less, a[i])
+            } else {
+                greater = append(greater, a[i])
+            }
+        }
+        cong := []int{}
+        cong = append(cong, qsort(less)...)
+        cong = append(cong, pivot)
+        cong = append(cong, qsort(greater)...)
+        return cong 
+    }
+}
+
 func arrshow(a []int) {
     for i := 0; i < len(a); i++ {
         fmt.Printf("%d ", a[i])
@@ -59,4 +108,12 @@ func main() {
     fmt.Printf("\n")
     t := 5
     fmt.Printf("sea(%d): %d\n", t, binsea(b, t))
+
+    c := []int{4,1,66,8,2,5,30,-10}
+    fmt.Printf("sum: %d\n", sum(c))
+    fmt.Printf("count: %d\n", count(c))
+    fmt.Printf("max: %d\n", maxi(c))
+    arrshow(c)
+    d := qsort(c)
+    arrshow(d)
 }
